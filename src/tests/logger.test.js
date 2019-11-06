0a58a9a603b7d09042f9abc25509849912b38f17
MiloNguyen95 <milonguyen95@outlook.com>
2019-11-06 18:46:37 +0700

    Add test to get high coverage

---
+++ src/tests/logger.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable no-console */
+import sinon from 'sinon';
+import logger from '../logger/logger';
+
+const assert = require('assert');
+
+
+describe('Logger should work properly', () => {
+  describe('#Existing object', () => {
+    it('should have an object for logger', () => {
+      assert.ok(logger, 'Logger is available');
+    });
+  });
+  describe('#Logger should show correct log', () => {
+    beforeEach(() => {
+      console.log = sinon.spy();
+    });
+
+    it('info logger should log correct info', () => {
+      logger.info('hello');
+      assert.ok(console.log.calledOnce);
+    });
+
+    it('debug logger should log correct debug', () => {
+      logger.debug('hello');
+      assert.ok(console.log.calledOnce);
+    });
+
+    it('error logger should log correct error', () => {
+      logger.error('hello');
+      assert.ok(console.log.calledOnce);
+    });
+  });
+});
