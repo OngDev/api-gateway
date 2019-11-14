4ba595873f335c3b47377c8b963e5de3003fbbb6
Duy Nguyen <milonguyen95@outlook.com>
2019-11-14 20:16:34 +0700

    Add user model to support service

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ import morgan from 'morgan';
 import { join } from 'path';
 import rfs from 'rotating-file-stream';
 import mongoose from 'mongoose';
+import session from 'express-session';
+import errorHandler from 'errorhandler';
 import logger from './src/logger/logger';
 import dbConfig from './src/configs/db.config';
 
 mongoose.Promise = global.Promise;
+
+// configure isProduction variable
+const isProduction = process.env.NODE_ENV === 'production';
 
 // defining the Express app
 const app = express();
@@ -31,6 +36,18 @@ const accessLogStream = rfs('access.log', {
 
 // adding morgan to log HTTP requests
 app.use(morgan('combined', { stream: accessLogStream }));
+
+// configure session
+app.use(session({
+  secret: 'vedgno',
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false,
+}));
+
+if (!isProduction) {
+  app.use(errorHandler());
+}
 
 // Connecting to the database
 mongoose
@@ -56,6 +73,31 @@ app.use('/api', require('./src/routes/routes').default);
 app.get('*', (req, res) => {
   logger.info('GET undefined routes');
   res.send('App works!!!!!');
+});
+
+// Error handlers & middlewares
+if (!isProduction) {
+  app.use((err, req, res) => {
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
+    });
+  });
+}
+
+app.use((err, req, res) => {
+  res.status(err.status || 500);
+
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  });
 });
 
 // starting the server
