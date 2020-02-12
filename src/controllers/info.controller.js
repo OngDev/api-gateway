366ac0769dca411b72bca2b74bdfc5f93e84759a
Bui Minh Chien <enpopo2101@gmail.com>
2020-02-12 21:46:36 +0700

    info controller

---
+++ src/controllers/info.controller.js
@@ -0,0 +1,46 @@
+import InfoService from '../services/info.service';
+
+const InfoController = {};
+
+InfoController.read = async (req, res) => {
+  try {
+    const infoData = await InfoService.read();
+    return res.status(200).json({ status: 200, data: infoData });
+  } catch (error) {
+    return res.status(400).json({ status: 400, message: error.message });
+  }
+};
+
+InfoController.update = async (req, res) => {
+  try {
+    const { key } = req.body;
+    const infoData = await InfoService.read(key, req.body);
+    return res
+      .status(200)
+      .json({ status: 200, data: infoData, message: 'Updated!' });
+  } catch (error) {
+    return res.status(400).json({ status: 400, message: error.message });
+  }
+};
+
+InfoController.create = async (req, res) => {
+  try {
+    const infoData = await InfoService.create(req.body);
+    return res
+      .status(200)
+      .json({ status: 200, data: infoData, message: 'Created!' });
+  } catch (error) {
+    return res.status(400).json({ status: 400, message: error.message });
+  }
+};
+
+InfoController.remove = async (req, res) => {
+  try {
+    const infoData = await InfoService.remove(req.body);
+    return res
+      .status(200)
+      .json({ status: 200, data: infoData, message: 'Created!' });
+  } catch (error) {
+    return res.status(400).json({ status: 400, message: error.message });
+  }
+};
